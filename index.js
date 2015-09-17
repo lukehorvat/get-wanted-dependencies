61d6aef30f3b86670c1a9c5c56a45ff1a61aaad7
Luke Horvat <lukehorvat@gmail.com>
2015-09-18 00:40:21 +1000

    Include dev dependencies

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+"use strict";
+
 var readPackageTree = require("read-package-tree");
+var objectAssign = require("object-assign");
 
 module.exports = function(dir, cb) {
   readPackageTree(dir, function(err, root) {
@@ -7,7 +10,7 @@ module.exports = function(dir, cb) {
       return;
     }
 
-    var packageDependencyVersions = root.package.dependencies;
+    var packageDependencyVersions = objectAssign(root.package.dependencies, root.package.devDependencies);
 
     var installedDependencyVersions = root.children.reduce(function(obj, child) {
       obj[child.package.name] = child.package.version;
